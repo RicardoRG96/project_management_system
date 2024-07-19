9e52456bbbaa77e438c59c4fdb8fcd5b758fb47c
RicardoRG96 <ricardoretamal10@gmail.com>
2024-07-19 15:34:41 -0400

    swagger installation and configuration

---
+++ swagger.js
@@ -0,0 +1,38 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+      openapi: '3.1.0',
+      info: {
+        title: 'Project Management System API',
+        description: "API endpoints for a Project Management System services documented on swagger",
+        contact: {
+          name: "Ricardo Retamal Guerrero",
+          email: "ricardoretamal10@gmail.com",
+          url: "https://github.com/DesmondSanctity/node-js-swagger"
+        },
+        version: '1.0.0',
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/",
+          description: "Local server"
+        },
+        {
+          url: "<your live url here>",
+          description: "Live server"
+        },
+      ]
+    },
+    apis: ['./routes/*.js'],
+}
+const swaggerSpec = swaggerJsdoc(options)
+function swaggerDocs(app, port) {
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+    app.get('/docs.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json')
+        res.send(swaggerSpec)
+    })
+}
+module.exports = swaggerDocs
